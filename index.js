015600aa117282e7633f997d6a5e5783e45cbfa6
luke <luke.edwards05@gmail.com>
2016-02-03 08:29:57 -0800

    remove default param for node-harmony

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const minify = require('html-minifier').minify
 
 module.exports = function () {
-  this.filter('htmlmin', (data, options = {}) => {
+  this.filter('htmlmin', (data, options) => {
+    options = options || {}
     return minify(data.toString(), options)
   })
 }
