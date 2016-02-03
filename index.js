5da1c9311f5ab46a249d9f74641035666314a5c2
luke <luke.edwards05@gmail.com>
2016-02-03 08:25:24 -0800

    remove object-assign, no need

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const assign = require('object-assign')
 const minify = require('html-minifier').minify
 
 module.exports = function () {
-  return this.filter('htmlmin', (data, options = {}) => {
-    return assign({ ext: '.html'}, minify(data.toString(), options))
+  this.filter('htmlmin', (data, options = {}) => {
+    return minify(data.toString(), options)
   })
 }
