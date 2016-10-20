478b6935862bb0ab0c65de89f1335839a23a4847
Luke Edwards <luke.edwards05@gmail.com>
2016-10-20 09:45:01 +0200

    include default config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const minify = require('html-minifier').minify;
+const defs = require('./config');
 
 module.exports = function () {
 	this.plugin('htmlmin', {}, function * (file, opts) {
-		opts = opts || {};
+		opts = Object.assign({}, defs, opts);
 		const min = minify(file.data.toString(), opts);
 		file.data = new Buffer(min);
 	});
