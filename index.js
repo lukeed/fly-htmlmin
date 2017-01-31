709e1fd6ff29f4591734d45b936c5c1596780170
Luke Edwards <luke.edwards05@gmail.com>
2017-01-31 13:10:39 -0800

    export as object definition

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const minify = require('html-minifier').minify;
+const {minify} = require('html-minifier');
 const defs = require('./config');
 
-module.exports = function () {
-	this.plugin('htmlmin', {}, function * (file, opts) {
+module.exports = {
+	name: 'htmlmin',
+	* func(file, opts) {
 		opts = Object.assign({}, defs, opts);
 		const min = minify(file.data.toString(), opts);
 		file.data = new Buffer(min);
-	});
+	}
 };
