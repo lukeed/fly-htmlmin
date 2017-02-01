1c4945fe63ac63191d53db5e7f6d522c0d59f9ad
Luke Edwards <luke.edwards05@gmail.com>
2017-01-31 16:10:25 -0800

    update tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const join = require('path').join;
-const test = require('tape').test;
+const {join} = require('path');
+const test = require('tape');
 const Fly = require('fly');
 
 const dir = join(__dirname, 'fixtures');
@@ -11,27 +11,28 @@ test('fly-htmlmin', t => {
 	t.plan(3);
 
 	const fly = new Fly({
-		plugins: [{
-			func: require('../')
-		}],
+		plugins: [
+			require('../'),
+			require('fly-clear')
+		],
 		tasks: {
-			a: function * () {
+			* foo(f) {
 				// #1
-				yield this.source(`${dir}/foo.html`).htmlmin().target(tmp);
-				const str1 = yield this.$.read(`${tmp}/foo.html`, 'utf8');
+				yield f.source(`${dir}/foo.html`).htmlmin().target(tmp);
+				const str1 = yield f.$.read(`${tmp}/foo.html`, 'utf8');
 				t.equal(str1, '<p title=blah id=moo>foo</p>', 'via defaults; minify html');
 
 				// #2
-				yield this.source(`${dir}/foo.html`).htmlmin({removeAttributeQuotes: 0}).target(tmp);
-				const str2 = yield this.$.read(`${tmp}/foo.html`, 'utf8');
+				yield f.source(`${dir}/foo.html`).htmlmin({removeAttributeQuotes: 0}).target(tmp);
+				const str2 = yield f.$.read(`${tmp}/foo.html`, 'utf8');
 				t.equal(str2, '<p title="blah" id="moo">foo</p>', 'via config; minify html');
 
-				yield this.clear(tmp);
+				yield f.clear(tmp);
 			}
 		}
 	});
 
-	t.ok('htmlmin' in fly, 'attach `htmlmin()` plugin to fly');
+	t.ok('htmlmin' in fly.plugins, 'attach `htmlmin()` plugin to fly');
 
-	fly.start('a');
+	fly.start('foo');
 });
